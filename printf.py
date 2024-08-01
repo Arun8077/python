0bb511de123b5dbf9aa9bb53ce0ee50a087bddaf
akssharma1994 <akssharma1994@gmail.com>
2024-08-02 04:51:45 +0530

    Use of double qotes and single quotes

--- printf.py
+++ printf.py
@@ -1,1 +1,12 @@
-print("hello world")
+print("hello world") # "" or '' used for combine muliple characters. 
+
+print('hello world') # "" or '' used for combine muliple characters. 
+
+print("My name is "Arun"") # Double "" inside "" ----> Syntax Error
+
+print("My name is 'Arun'")  # Single '' inside "" ----> Output 'Arun'
+
+print('My name is 'Arun'')  # Single '' inside '' ----> Output 'Syntax Error'
+
+print('My name is "Arun"')  # Double "" inside '' ----> Output "Arun"
+
