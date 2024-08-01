b620b619ad08172682d12a73937d2441907bf28d
akssharma1994 <akssharma1994@gmail.com>
2024-08-02 05:18:56 +0530

    Use of Escape Sequence

---
+++ escape_sequences.py
@@ -0,0 +1,31 @@
+#  \" Escape sequence.
+print("My name is \"Arun")    # \" will print the " as it is within ""
+
+# \' Escape sequence.
+
+print('My name is \'Arun')    # \' will print the ' as it is within ''
+
+# \n Escape sequence.
+
+print("Line A")
+print("Line B")
+
+print("Line A \n Line B")
+
+print("Line A \nLine B \n Line C")
+
+# \t Escape Sequence
+
+print("Line A \t Line B")
+
+print("Line A \tLine B")
+
+# \\ Escape Sequence
+
+print("this is \ backslash")
+
+
+print("this is backslash\") # Syntax Error due to \" 
+
+print("this is backslash \\")
+
