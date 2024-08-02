95a9c912ba4e6904c7341a1148c3421686baba25
akssharma1994 <akssharma1994@gmail.com>
2024-08-02 05:31:10 +0530

    Use of Escape Sequence

--- printf.py
+++ printf.py
@@ -1,15 +1,13 @@
-# print("hello world") # "" or '' used for combine muliple characters. 
+print("hello world") # "" or '' used for combine muliple characters. 
 
-# print('hello world') # "" or '' used for combine muliple characters. 
+print('hello world') # "" or '' used for combine muliple characters. 
 
-# print("My name is "Arun"") # Double "" inside "" ----> Syntax Error
+print("My name is "Arun"") # Double "" inside "" ----> Syntax Error
 
-# print("My name is 'Arun'")  # Single '' inside "" ----> Output 'Arun'
+print("My name is 'Arun'")  # Single '' inside "" ----> Output 'Arun'
 
-# print('My name is 'Arun'')  # Single '' inside '' ----> Output 'Syntax Error'
+print('My name is 'Arun'')  # Single '' inside '' ----> Output 'Syntax Error'
 
-# print('My name is "Arun"')  # Double "" inside '' ----> Output "Arun"
+print('My name is "Arun"')  # Double "" inside '' ----> Output "Arun"
 
-# print("My name is \"Arun")    # \" will print the " as it is within ""
 
-print('My name is \'Arun')    # \' will print the ' as it is within ''
