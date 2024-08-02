a30ffdb30ec8fd8d9056588663d7b1bdcdf7b178
akssharma1994 <akssharma1994@gmail.com>
2024-08-03 02:01:32 +0530

    Escape Sequence as normal text

---
+++ escape_sequence_as_normal_text.py
@@ -0,0 +1,21 @@
+# Output: Line A \n Line B
+
+print("Line A \\n Line B")
+
+# Output: Line A \t Line B
+
+print("Line A \\t Line B")
+
+# Output: This is double backslash \\
+
+print("This is double backslash \\\\")
+
+# Output: This is 4 backslash \\\\
+
+print("This is 4 backslash \\\\\\\\")
+
+# Output \' \""
+print(" \\\' \\\"")
+
+# /' -----> '
+# // -----> /
